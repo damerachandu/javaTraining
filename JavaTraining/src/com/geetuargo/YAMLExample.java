e479ee31f76f637555686778073613a5224fed0e
Ramachandra.Damera <Ramachandra.Damera@YH1000164LT.Yash.com>
2021-08-23 17:42:42 +0530

    test-github

--- JavaTraining/src/com/geetuargo/YAMLExample.java
+++ JavaTraining/src/com/geetuargo/YAMLExample.java
@@ -1,22 +1,22 @@
 package com.geetuargo;
 import java.io.File;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 public class YAMLExample {
 
 	public static void main(String[] args) {
-		DatabaseConfig dbconfig = new DatabaseConfig();
-		dbconfig.setDbname("newdb");
-		dbconfig.setDriver("newdriver");
-		dbconfig.setPort(6601);
-		dbconfig.setUsername("appuser");
-		dbconfig.setPassword("apppassword");
-		ApplicationConfig appConfig = new ApplicationConfig();
-		appConfig.setAuthor("Tom");
-		appConfig.setDatabase(dbconfig);
-		ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
-		objectMapper.writeValue(new File("src/main/resources/application1.yaml"), appConfig);
-	
+	//	DatabaseConfig dbconfig = new DatabaseConfig();
+//		dbconfig.setDbname("newdb");
+//		dbconfig.setDriver("newdriver");
+//		dbconfig.setPort(6601);
+//		dbconfig.setUsername("appuser");
+//		dbconfig.setPassword("apppassword");
+//		ApplicationConfig appConfig = new ApplicationConfig();
+//		appConfig.setAuthor("Tom");
+//		appConfig.setDatabase(dbconfig);
+//		ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
+//		objectMapper.writeValue(new File("src/main/resources/application1.yaml"), appConfig);
+//	
 
 	}
 
